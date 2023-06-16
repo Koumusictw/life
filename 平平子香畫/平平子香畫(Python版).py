1639c5fb54f3f8878def132f50b2ace8db13f363
小冰 <xiaopengjibai@gmail.com>
2023-06-17 00:24:21 +0800

    公告
    真香??
    (jpg檔也要下載，因為沒載會讀不到)

---
+++ 平平子香畫/平平子香畫(Python版).py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import cv2                    # 匯入 OpenCV 函式庫
+img = cv2.imread('d4bb727feb0e7f6844c000d63b2c2fbb.jpg')  # 讀取圖片
+cv2.imshow('Padko',img)        # 賦予開啟的視窗名稱，開啟圖片
+cv2.waitKey(0)                # 設定 0 表示不要主動關閉視窗
+#由小冰製作
+#第一次用Jupyter Notebook寫
+
+
+# In[ ]:
+
+
+
+
